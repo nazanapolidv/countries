84fb029a54e3acf1c71cfd7c55dc390f7545d8c0
Nazareno Napoli <nazareno.napoli@davinci.edu.ar>
2026-01-11 02:43:02 -0300

    Add ECONOMIC_BLOCS constant with country groupings

---
+++ constants/blocs.ts
@@ -0,0 +1,22 @@
+export const ECONOMIC_BLOCS: Record<string, string[]> = {
+    bloc_eu: [ // European Union
+        "AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FIN",
+        "FRA", "DEU", "GRC", "HUN", "IRL", "ITA", "LVA", "LTU", "LUX",  
+        "MLT", "NLD", "POL", "PRT", "ROU", "SVK", "SVN", "ESP", "SWE"
+    ],
+    bloc_au: [ // African Union
+        "DZA", "AGO", "BEN", "BWA", "BFA", "BDI", "CMR", "CPV", "CAF", "TCD",
+        "COM", "COG", "CIV", "DJI", "EGY", "GNQ", "ERI", "SWZ", "ETH", "GAB",
+        "GMB", "GHA", "GIN", "GNB", "KEN", "LSO", "LBR", "LBY", "MDG", "MWI",
+        "MLI", "MRT", "MUS", "MAR", "MOZ", "NAM", "NER", "NGA", "RWA", "STP",
+        "SEN", "SYC", "SLE", "SOM", "ZAF", "SSD", "SDN", "TZA", "TGO", "TUN",
+        "UGA", "ZMB", "ZWE"
+    ],
+    bloc_usan: [ // Union of South American Nations
+        "ARG", "BOL", "BRA", "CHL", "COL", "ECU", "GUY", "PRY", "PER", "SUR",
+        "URY", "VEN"
+    ],
+    bloc_pa: [ // Pacific Alliance
+        "CHL", "COL", "MEX", "PER"
+    ]
+}
